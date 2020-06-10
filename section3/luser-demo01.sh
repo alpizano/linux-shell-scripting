ca44a9973e7a56024b46a400c9dd818367ab3dee
alpizano <apizano@purdue.edu>
2020-06-10 01:36:53 -0500

    More tricks with variables

--- section3/luser-demo01.sh
+++ section3/luser-demo01.sh
@@ -2,8 +2,37 @@
 
 # This script displays various information to the screen
 
-# Display 'Hello'
+# Display 'Hello World'
 echo 'Hello World'
 
 # Assign a value to a variable
 WORD='script'
+
+# Display that value using the variable
+echo "$WORD"
+
+# Demonstrate that single quotes cause variables to NOT get expanded
+echo '$WORD'
+
+# combine the variable with hard coded text
+echo "This is a shell $WORD"
+
+# Display the contents of the variable using an alternative syntax
+echo "This is a shell ${WORD}"
+
+# Append text to the variable
+echo "${WORD}ing is fun"
+
+# Show how NOT to append text to a variable
+# This does not work
+echo "$WORDing is fun!"
+
+# Create a new variable 
+ENDING='ed'
+
+# Combine the two variables
+echo "This is ${WORD}${ENDING}"
+
+ENDING='ing'
+
+echo "${WORD}${ENDING} is fun"
